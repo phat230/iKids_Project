5520c64b500e0f5dbac36acad6755ea4e4966a45
phat230 <phay123321@gmail.com>
2026-06-15 16:34:23 +0700

    Mô tả thay đổi

--- ikids_mobile/lib/core/config.dart
+++ ikids_mobile/lib/core/config.dart
@@ -1,7 +1,7 @@
 // lib/core/config.dart
 class AppConfig {
   // Bắt buộc dùng 10.0.2.2 cho Máy ảo Android (Emulator)
-  static const String apiUrl = "http://10.0.2.2:8000"; 
+  static const String apiUrl = "https://ikids-project.onrender.com"; 
   
   static const int connectTimeout = 30; // giây
   
