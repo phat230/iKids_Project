13fc9e4c7272a599ac60a080133f1f80891bd1f4
phat230 <phay123321@gmail.com>
2026-06-22 15:26:52 +0700

    Mô tả thay đổi

--- ikids_mobile/lib/core/config.dart
+++ ikids_mobile/lib/core/config.dart
@@ -1,10 +1,11 @@
 // lib/core/config.dart
 class AppConfig {
-  // Địa chỉ Backend thực tế trên Render
-  static const String apiUrl = "https://ikids-backend-api.onrender.com";  
+  static const String baseUrl = "https://ikids-backend-api.onrender.com"; 
+  
   static const String apiUrl = baseUrl; 
   
-static const int connectTimeout = 60;  
+  static const int connectTimeout = 60;  
+
   // ================= CÁC MODULE API CHÍNH =================
   static const String apiAuth = "$baseUrl/api/auth";
   static const String apiTv1 = "$baseUrl/api/tv1";       
